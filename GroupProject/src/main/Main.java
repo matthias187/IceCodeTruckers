2d2fd967f913150fd14612c701ffd37c95ae7797
John Halmaghi <john.g.halmaghi@gmail.com>
2018-11-20 19:09:56 -0800

    Revert "Created FXML and Controller classes. Basic Menu navigation."
    This reverts commit 9f7bfe702a6896149ba9fcba54e639c889afb21f.

--- GroupProject/src/main/Main.java
+++ GroupProject/src/main/Main.java
@@ -1,35 +1,56 @@
 package main;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import java.io.IOException;
 
 
 public class Main extends Application
 {
-    public static int MENU_WIDTH = 1024;
-    public static int MENU_HEIGHT = 640;
-    public static Stage stage;
     public static void main( String[] args )
     {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws IOException {
-        this.stage = primaryStage;
-        Parent root = FXMLLoader.load(getClass().getResource("fxmls/login.fxml"));
-        Scene scene = new Scene(root, MENU_WIDTH, MENU_HEIGHT);
-        primaryStage.setTitle("Login");
+    public void start(Stage primaryStage){
+        primaryStage.setTitle("Ice Code Truckers");
+
+        Button login = new Button("Log In");
+        login.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                //Do login stuff
+            }
+        });
+
+        BorderPane layout = new BorderPane();
+        VBox vbox = new VBox();
+        vbox.setSpacing(10);
+        vbox.setPadding(new Insets(10, 50, 50, 50));
+        TextField username = new TextField();
+        username.setPromptText("Username");
+        TextField password = new TextField();
+        password.setPromptText("Password");
+
+        //Set initial focus to the login button
+        Platform.runLater(() -> login.requestFocus());
+        vbox.setAlignment(Pos.CENTER);
+        vbox.getChildren().add(username);
+        vbox.getChildren().add(password);
+        vbox.getChildren().add(login);
+        layout.setRight(vbox);
+        Scene scene = new Scene(layout, 1024, 640);
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
-
-
-    public static void loadNewFXML(Stage stage, String fxml){
-
     }
 }
