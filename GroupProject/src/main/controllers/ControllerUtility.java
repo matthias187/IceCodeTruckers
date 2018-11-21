9f7bfe702a6896149ba9fcba54e639c889afb21f
JHalmaghi <44728275+JHalmaghi@users.noreply.github.com>
2018-11-20 18:48:28 -0800

    Created FXML and Controller classes. Basic Menu navigation.

---
+++ GroupProject/src/main/controllers/ControllerUtility.java
@@ -0,0 +1,38 @@
+package main.controllers;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import main.Main;
+
+import java.io.IOException;
+import java.util.Stack;
+
+public class ControllerUtility {
+
+    public ControllerUtility(){
+        if (fxmlsLoaded.size() == 0){
+            fxmlsLoaded.push("login.fxml");
+        }
+    }
+
+    protected static Stack<String> fxmlsLoaded = new Stack<>();
+
+
+    public void back() throws IOException {
+        fxmlsLoaded.pop();
+        Parent root = FXMLLoader.load(getClass().getResource(fxmlsLoaded.pop()));
+        Scene scene = new Scene(root, Main.MENU_WIDTH, Main.MENU_HEIGHT);
+        Main.stage.setScene(scene);
+        Main.stage.show();
+    }
+
+    public void loadNewFXML(Stage stage, String fxml) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource(fxml));
+        Scene scene = new Scene(root, Main.MENU_WIDTH, Main.MENU_HEIGHT);
+        stage.setScene(scene);
+        fxmlsLoaded.push(fxml);
+        stage.show();
+    }
+}
