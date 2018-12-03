6771310427ba05ec7ad95b64a06eed5cd236f4f5
John Halmaghi <john.g.halmaghi@gmail.com>
2018-12-02 17:20:49 -0800

    Edited place order (database needs fixing in order to work)

--- GroupProject/src/main/controllers/PlaceOrderController.java
+++ GroupProject/src/main/controllers/PlaceOrderController.java
@@ -1,27 +1,67 @@
 package main.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import main.DatabaseUtility;
 
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Comparator;
 
 public class PlaceOrderController {
+
+    private final ObservableList<Integer> quantities =
+            FXCollections.observableArrayList(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15);
     @FXML
     private Button placeOrder;
     @FXML
     private Button back;
     @FXML
     private Hyperlink signOut;
+    @FXML
+    private ComboBox vanillaQuant;
+    @FXML
+    private ComboBox chocolateQuant;
+    @FXML
+    private ComboBox strawberryQuant;
+
 
 
     private ControllerUtility utility = new ControllerUtility();
+    private DatabaseUtility dbUtil = new DatabaseUtility();
+
 
     @FXML
-    public void placeOrderPressed() throws IOException {
+    void initialize(){
+        vanillaQuant.getItems().clear();
+        chocolateQuant.getItems().clear();
+        strawberryQuant.getItems().clear();
+        vanillaQuant.setItems(quantities);
+        chocolateQuant.setItems(quantities);
+        strawberryQuant.setItems(quantities);
+        vanillaQuant.getSelectionModel().selectFirst();
+        chocolateQuant.getSelectionModel().selectFirst();
+        strawberryQuant.getSelectionModel().selectFirst();
+
+    }
+
+    @FXML
+    public void placeOrderPressed() throws IOException, SQLException {
+        int vanillaQuant = (int) this.vanillaQuant.getValue();
+        int chocolateQuant = (int) this.chocolateQuant.getValue();
+        int strawberry = (int) this.strawberryQuant.getValue();
+        String placeOrderInsert = "INSERT INTO Orders (Customer_ID, Customer_name) VALUES ("+ dbUtil.getCurrentUser().getUserID()+ ", \'"+ dbUtil.getCurrentUser().getFirsName()+" "+dbUtil.getCurrentUser().getLastName()+"\');";
+        dbUtil.insertDatabase(placeOrderInsert);
+        utility.showAlert("Order Confirmation", "Your order has been successfully placed!");
         Stage stage = (Stage) placeOrder.getScene().getWindow();
-        utility.loadNewFXML(stage, "../fxmls/confirmation.fxml");
+        utility.loadNewFXML(stage, "../fxmls/user_landing_page.fxml");
     }
 
     @FXML
